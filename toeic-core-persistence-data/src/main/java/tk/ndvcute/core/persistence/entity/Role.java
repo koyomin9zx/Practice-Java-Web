9a458c29da048027f9aab1f27f8695b88c17154f
koyomin9xz <kaiznguyen98@gmail.com>
2020-03-24 09:45:44 +0700

    init entity

--- toeic-core-persistence-data/src/main/java/tk/ndvcute/core/persistence/entity/Role.java
+++ toeic-core-persistence-data/src/main/java/tk/ndvcute/core/persistence/entity/Role.java
@@ -1,63 +1,9 @@
 package tk.ndvcute.core.persistence.entity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
-@Table(name="user")
+@Table(name="roleid")
 public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer userId;
-
-    @Column(name = "name")
-    private String name;
-
-    @Column(name = "password")
-    private  String password;
-
-    @Column(name = "fullname")
-    private String fullName;
-
-    @Column(name = "createdate")
-    private  String createDate;
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(String createDate) {
-        this.createDate = createDate;
-    }
 }
--- toeic-core-persistence-data/src/main/java/tk/ndvcute/core/persistence/entity/User.java
+++ toeic-core-persistence-data/src/main/java/tk/ndvcute/core/persistence/entity/User.java
@@ -1,4 +1,63 @@
 package tk.ndvcute.core.persistence.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private  String password;
+
+    @Column(name = "fullname")
+    private String fullName;
+
+    @Column(name = "createdate")
+    private  String createDate;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
 }
