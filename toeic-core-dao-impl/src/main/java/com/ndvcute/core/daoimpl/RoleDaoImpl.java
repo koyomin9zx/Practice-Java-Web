a9fe439c4b37f21c32ca971844f408fffc834c9a
koyomin9xz <kaiznguyen98@gmail.com>
2020-04-01 09:15:58 +0700

    init RoleDaoImpl

---
+++ toeic-core-dao-impl/src/main/java/com/ndvcute/core/daoimpl/RoleDaoImpl.java
@@ -0,0 +1,10 @@
+package com.ndvcute.core.daoimpl;
+
+import com.ndvcute.core.dao.RoleDao;
+import com.ndvcute.core.data.daoimpl.AbstractDao;
+import com.ndvcute.core.persistence.entity.RoleEntity;
+
+
+public class RoleDaoImpl extends AbstractDao<Integer,RoleEntity> implements RoleDao {
+
+}
