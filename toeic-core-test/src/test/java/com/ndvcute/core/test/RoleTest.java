e5e63e36dc8a8ec88dac7b9291ab9ea051153543
koyomin9xz <kaiznguyen98@gmail.com>
2020-04-01 09:17:08 +0700

    init test

--- toeic-core-test/src/test/java/com/ndvcute/core/test/RoleTest.java
+++ toeic-core-test/src/test/java/com/ndvcute/core/test/RoleTest.java
@@ -1,4 +1,16 @@
 package com.ndvcute.core.test;
 
+import com.ndvcute.core.dao.RoleDao;
+import com.ndvcute.core.daoimpl.RoleDaoImpl;
+import com.ndvcute.core.persistence.entity.RoleEntity;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 public class RoleTest {
+    @Test
+    public void checkfindAll() {
+        RoleDao roleDao=new RoleDaoImpl();
+        List<RoleEntity> list= roleDao.findAll();
+    }
 }
