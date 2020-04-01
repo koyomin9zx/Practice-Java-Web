eac6405dc23ac325646a91c2d0b56de50f68c56e
koyomin9xz <kaiznguyen98@gmail.com>
2020-04-01 09:15:44 +0700

    init RoleDao

---
+++ toeic-core-dao/src/main/java/com/ndvcute/core/dao/RoleDao.java
@@ -0,0 +1,7 @@
+package com.ndvcute.core.dao;
+
+import com.ndvcute.core.data.dao.GenericDao;
+import com.ndvcute.core.persistence.entity.RoleEntity;
+
+public interface RoleDao extends GenericDao<Integer, RoleEntity> {
+}
