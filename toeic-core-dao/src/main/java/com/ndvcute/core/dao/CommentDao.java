19b5b370a314853d3d8d4302ad34febe6e977bb2
koyomin9xz <kaiznguyen98@gmail.com>
2020-04-04 11:15:50 +0700

    init dao database

---
+++ toeic-core-dao/src/main/java/com/ndvcute/core/dao/CommentDao.java
@@ -0,0 +1,7 @@
+package com.ndvcute.core.dao;
+
+import com.ndvcute.core.data.dao.GenericDao;
+import com.ndvcute.core.persistence.entity.CommentEntity;
+
+public interface CommentDao extends GenericDao<Integer, CommentEntity> {
+}
---
+++ toeic-core-dao/src/main/java/com/ndvcute/core/dao/ListenGuideLineDao.java
@@ -0,0 +1,7 @@
+package com.ndvcute.core.dao;
+
+import com.ndvcute.core.data.dao.GenericDao;
+import com.ndvcute.core.persistence.entity.ListenGuidelineEntity;
+
+public interface ListenGuideLineDao extends GenericDao<Integer, ListenGuidelineEntity> {
+}
